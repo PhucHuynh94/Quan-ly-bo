5742e2a67cff81e28fd8047108476024dac3271f
Nhat Bang <dinhnhatbang@gmail.com>
2016-03-20 13:45:43 +0700

    hello Nuoi Bo S

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>Nuôi Bò Số</title>
+</head>
+<body>
+	<h1>Nuôi Bò Số</h1>
+</body>
+</html>
